fa9ace1605bfb6c2eddbabff328427d1e96f0183
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-06-11 11:38:46 +0530

    changed TaskTile from StatelessWidget to StatefulWidget lifted the state up to have a callBack fn to toggleCheckboxState
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  var isChecked = false;
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: const Text('This is task'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (_) {},
+      title: Text(
+        'This is task',
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
       ),
+      trailing: TaskCheckBox(
+        checkBoxState: isChecked,
+        toggleCheckboxState: (checkBoxState) => setState(() => isChecked = checkBoxState!),
+      ),
+    );
+  }
+}
+
+class TaskCheckBox extends StatelessWidget {
+  final bool checkBoxState;
+  final Function(bool?) toggleCheckboxState;
+
+  const TaskCheckBox({
+    super.key,
+    required this.checkBoxState,
+    required this.toggleCheckboxState,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: checkBoxState,
+      onChanged: toggleCheckboxState,
     );
   }
 }
