01589e7e3043252e1f3da4af839a4650632166c0
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-06-13 13:51:23 +0530

    created tasks_data, lifted up the state of tasks list wrapped MaterialApp in ChangeNotifierProvider widget to provides change notification to its listeners.
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import 'models/task_data.dart';
 import 'screens/tasks_screen.dart';
 
 void main() => runApp(const MyApp());
@@ -9,8 +11,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: TasksScreen(),
+    return ChangeNotifierProvider(
+      create: (context) => TaskData(),
+      child: MaterialApp(
+        home: TasksScreen(),
+        debugShowCheckedModeBanner: false,
+      ),
     );
   }
 }
---
+++ lib/models/task_data.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/foundation.dart';
+
+import 'task.dart';
+
+class TaskData extends ChangeNotifier {
+  List<Task> _tasks = [
+    Task(title: 'Buy Milk'),
+    Task(title: 'Buy Groceries'),
+    Task(title: 'Buy anything'),
+  ];
+
+  List<Task> get tasks {
+    return [..._tasks];
+  }
+
+  int get tasksCount {
+    return _tasks.length;
+  }
+
+  void addTask(String newTaskTite) {
+    final task = Task(title: newTaskTite);
+    _tasks.add(task);
+    notifyListeners();
+  }
+
+  void updateTask(Task task) {
+    task.toggleTaskCompleted();
+    notifyListeners();
+  }
+}
